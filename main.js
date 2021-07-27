82dd545ce8977e1e7cd8a2c3fc65b39a4b5f9253
Malik Washington <kingmalikwashington@gmail.com>
2021-07-27 12:59:26 -0500

    cookiestandex

---
+++ main.js
@@ -0,0 +1,64 @@
+// alert("Loaded your javascript");
+
+// we need to be able to keep up with sale number by the hr so we need to have a list of hours
+const biznessHours = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+  "8pm",
+];
+
+// define an object literal
+let Seattle = {
+  // identify props
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookiessale: 6.3,
+  customersPerHour: [],
+  cookiesPerHour: [],
+  totalDailyCookies: 0,
+  // Determine per hour open, how many customers to expect?
+};
+
+function getRandomNumberOfCustomersGivenARange(minCustomers) {
+  return
+}
+
+// let tokyo = {
+//     // identify props
+//     minCustumers: 3,
+//     maxCustumers: 24,
+//     avgCookies: 1.2
+// }
+
+// let dubai = {
+//     // identify props
+//     minCustumers: 11,
+//     maxCustumers: 38,
+//     avgCookies: 37
+// }
+
+// let paris = {
+//     // identify props
+//     minCustumers: 20,
+//     maxCustumers: 38,
+//     avgCookies: 3.3
+// }
+
+// let lima = {
+//     // identify props
+//     minCustumers: 2,
+//     maxCustumers: 16,
+//     avgCookies: 4.6
+// }
